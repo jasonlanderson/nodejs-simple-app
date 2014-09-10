640dcca11b8b09f1fa0943f4c8cb0c23b7006efb
JASON L. ANDERSON <jason.anderson@us.ibm.com>
2014-09-10 15:09:42 -0700

    Printing out vcap variables

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 /*jshint node:true*/
 
+var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+
+appInfoStr = JSON.stringify(appInfo, undefined, 2)
+servicesStr = JSON.stringify(services, undefined, 2)
+
 var http = require('http');
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
+  response.end("Below is your app and service info\n App Info: " + appInfoStr + "\n Service Info: " + servicesStr + "\n");
 });
-
-var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
-var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
 
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
 var host = (process.env.VCAP_APP_HOST || 'localhost');
